d14b4ec252da97278398777bab14f4474996206a
Nadine Cumberbatch <nadine.cumberbatch@bts.tech>
2020-02-20 18:29:17 +0100

    About Us page structure

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import '../AboutUs.css'
 
 class AboutUs extends Component {
-
-
 
     render () {
 
         return (
-            <div>
-                <h1>About Us</h1>
+            <div className = 'team'>
+                <div className = 'title'>About Us</div>
+                <div className = 'title'>Team</div>
+                <div className = 'member'>
+                    <div className = 'photo'>LouPhoto</div>
+                    <div className = 'tagline'>LouName</div>
+                    <div className = 'position'>LouPos</div>
+                    <div className = 'bio'>Loubio</div>
+                </div>
+                <div className = 'member'>
+                    <div className = 'photo'>KanyaPhoto</div>
+                    <div className = 'tagline'>KanyaName</div>
+                    <div className = 'position'>KanyaPos</div>
+                    <div className = 'bio'>Kanyabio</div>
+                </div>
+                <div className = 'member'>
+                    <div className = 'photo'>
+                        <img src="/img/nadinepic.jpg" alt="nadine picture" className="img" />
+                    </div>
+                    <div className = 'tagline'>NanoName</div>
+                    <div className = 'position'>NanoPos</div>
+                    <div className = 'bio'>Nanobio</div>
+                </div>
+                <div className = 'member'>
+                    <div className = 'photo'>
+                        <img src="/img/nadinepic.jpg" alt="nadine picture" className="img" />
+                    </div>
+                    <div className = 'tagline'>NadineName</div>
+                    <div className = 'position'>NadinePos</div>
+                    <div className = 'bio'>Nadinebio</div>
+                </div>
+
             </div>
         );
 
