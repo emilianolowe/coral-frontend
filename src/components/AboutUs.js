41807405b903158b89be6bf43a0df4c0b3ac6b2c
Nadine Cumberbatch <nadine.cumberbatch@bts.tech>
2020-02-20 18:51:49 +0100

    nadine

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,18 +9,23 @@ class AboutUs extends Component {
             <div className = 'team'>
                 <div className = 'title'>About Us</div>
                 <div className = 'title'>Team</div>
+                
                 <div className = 'member'>
-                    <div className = 'photo'>LouPhoto</div>
+                    <div className = 'photo'>
+                    <img src="/img/lourenco.jpg" alt="lourenco picture" className="img" />
+                    </div>
                     <div className = 'tagline'>LouName</div>
                     <div className = 'position'>LouPos</div>
                     <div className = 'bio'>Loubio</div>
                 </div>
+
                 <div className = 'member'>
                     <div className = 'photo'>KanyaPhoto</div>
                     <div className = 'tagline'>KanyaName</div>
                     <div className = 'position'>KanyaPos</div>
                     <div className = 'bio'>Kanyabio</div>
                 </div>
+
                 <div className = 'member'>
                     <div className = 'photo'>
                         <img src="/img/nadinepic.jpg" alt="nadine picture" className="img" />
@@ -29,6 +34,7 @@ class AboutUs extends Component {
                     <div className = 'position'>NanoPos</div>
                     <div className = 'bio'>Nanobio</div>
                 </div>
+
                 <div className = 'member'>
                     <div className = 'photo'>
                         <img src="/img/nadinepic.jpg" alt="nadine picture" className="img" />
