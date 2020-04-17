b93bf9bc73d8f491eb9825a432f074590a8fb9f5
Kanya <kanyalyons@gmail.com>
2020-04-17 17:37:00 +0200

    added hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Hero extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Renting Reinvented</h1>
+                <img src = "img/apartmentblueaccent.jpeg" alt = "apartment with blue accent"/>
+            </div>
+        );
+    }
+
+}
+
+export default Hero;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Jumbotron from "react-bootstrap/Jumbotron";
+import Hero from "./Hero";
 
 class HomePage extends Component {
   render() {
     return (
-      <Jumbotron fluid>
-        <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
-        </Container>
-      </Jumbotron>
+      <div className = "container">
+        <Hero/>
+        
+      </div>
     );
   }
 }
