4f956b05dd45324e03f33361086b0232d38bb3c8
ltreven <ltreven@gmail.com>
2020-04-24 19:47:33 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
         return (
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Coral</a>
+                    <a className="navbar-brand" href="/home">Coral</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -14,10 +14,10 @@ class NavBar extends Component {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Home</a>
+                                <a className="nav-link" href="/home">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About us</a>
+                                <a className="nav-link" href="/aboutus">About us</a>
                             </li>
                         </ul>
                     </div>
