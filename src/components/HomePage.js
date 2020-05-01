3da2b7292733b2b837a5978fc78a964c56c65061
Kanya <kanyalyons@gmail.com>
2020-05-01 16:23:53 +0200

    add hero full width styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,12 +5,12 @@ import WhyCoralCard from "./WhyCoralCard";
 class HomePage extends Component {
   render() {
     return (
-      <div className = "container">
-        <Hero heroHeader = "Rent your dream home!"
-        subHeader = "No realtor fees. Legal deposit only, nothing more."
-        imgSrc = "img/IMG_7193.jpeg"
-        imgAlt = "Barcelona building facade"/>
-        <WhyCoralCard/>
+      <div>
+        <Hero heroHeader="Rent your dream home!"
+          subHeader="No realtor fees. Legal deposit only, nothing more."
+          imgSrc="img/IMG_7193.jpeg"
+          imgAlt="Barcelona building facade" />
+        <WhyCoralCard />
       </div>
     );
   }
