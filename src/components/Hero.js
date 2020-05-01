6e6051fc101c1252ae4af514d88dfcc713222a73
ltreven <ltreven@gmail.com>
2020-05-01 16:30:33 +0200

    fixed hero line break

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,10 @@ class Hero extends Component {
     render() {
         return (
             <div className="hero">
-                <h1>{this.props.heroHeader}</h1>
-                <h2>{this.props.subHeader}</h2>
+                <div>
+                    <h1>{this.props.heroHeader}</h1>
+                    <h2>{this.props.subHeader}</h2>
+                </div>
             </div>
         );
     }
