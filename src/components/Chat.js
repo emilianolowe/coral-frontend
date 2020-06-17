6a3694f1eee73d2102f1a3b8ea71c850955470a3
ltreven <ltreven@gmail.com>
2020-06-17 19:23:22 +0200

    Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Card from "./PropertyCard";
 import { getProperty, getUserId } from './PropertiesDAO'
 import Cookies from 'universal-cookie'
+import ChatMessage from './ChatMessage'
 
 class Chat extends Component {
 
@@ -14,12 +14,12 @@ class Chat extends Component {
             me: null,
             recipient: null,
         }
-        
+
         const cookies = new Cookies()
         const myMail = cookies.get("coraluser")
 
-        getProperty(this.props.propertyId, property => this.setState({property}))
-        getUserId(myMail, id => this.setState({me: id}) )
+        getProperty(this.props.propertyId, property => this.setState({ property }))
+        getUserId(myMail, id => this.setState({ me: id }))
 
     }
 
@@ -27,17 +27,28 @@ class Chat extends Component {
         const property = {}
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-7">
-                        <p>date</p>
-                        <p>Chat message</p>
-                        <p>Chat message</p>
-                        <p>Chat message</p>
-                    </div>
-                    <div className="col-5">
-                        <Card property={property} />
-                    </div>
+                <div className="more-seperator"><b>Wed, 16 June</b></div>
+
+                <ChatMessage orientation="left" img="img/kanya.png" time="12:00 PM" from="Kanya Lyons" message="Hey Nano, let's talk about this property" />
+                <ChatMessage orientation="right" img="img/nano.png" time="05:00 PM" from="Emiliano Lowe" message="How are you, K?" />
+                <ChatMessage orientation="left" img="img/kanya.png" time="12:00 PM" from="Kanya Lyons" message="I'm fine. Just want to rent your home" />
+                <ChatMessage orientation="right" img="img/nano.png" time="05:00 PM" from="Emiliano Lowe" message="Ok. Let's negotiate" />
+
+                <div className="more-seperator"><b>Wed, 17 June</b></div>
+
+                <ChatMessage orientation="left" img="img/kanya.png" time="12:00 PM" from="Kanya Lyons" message="Hey Nano, You know what? Maybe not" />
+                <ChatMessage orientation="right" img="img/nano.png" time="05:00 PM" from="Emiliano Lowe" message="But why ?" />
+                <ChatMessage orientation="left" img="img/kanya.png" time="12:00 PM" from="Kanya Lyons" message="I don't want to live there anymore" />
+                <ChatMessage orientation="right" img="img/nano.png" time="05:00 PM" from="Emiliano Lowe" message="Ok. I understand..." />
+
+                <br/><br/>
+                <div className="container">
+                    <form className="form-inline d-flex">
+                        <input type="text" className="form-control w-75" placeholder="Type your message here" />
+                        <input type="submit" className="btn btn-info" value="Send" />
+                    </form>
                 </div>
+                <br/><br/><br/><br/>
             </div>
         )
     }
---
+++ src/components/ChatMessage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const ChatMessage = props => {
+
+  let col1, col2, orientation, col1Size, col2Size
+
+  const avatar = (<div className="chat-avatar">
+    <img src={props.img} alt="prospect" />
+  </div>)
+
+  const message = (
+    <p>
+      <small>{props.time}</small>
+      <br />
+      <strong>{props.from}</strong>
+      <br />
+      {props.message}
+    </p>
+  )
+
+  if (props.orientation === "left") {
+    col1 = avatar
+    col2 = (
+      <div className="col text-left">
+        {message}
+      </div>
+    )
+    orientation = "d-flex justify-content-start"
+    col1Size = "col-2"
+    col2Size = "col-10"
+
+  } else {
+    col1 = (
+      <div className="col text-right">
+        {message}
+      </div>
+    )
+    col2 = avatar
+    orientation = "d-flex justify-content-end"
+    col1Size = "col-10"
+    col2Size = "col-2"
+  }
+
+
+  return (
+    <div className={orientation}>
+      <div className="container">
+        <div className="row">
+          <div className={col1Size}>
+            {col1}
+          </div>
+          <div className={col2Size}>
+            {col2}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ChatMessage;
